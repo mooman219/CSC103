4ceae18922251f7add931ea80bcb6f4c4580a2e8
Joe <mooman219@gmail.com>
2013-11-18 16:24:28 -0500

    Finish lab 4

--- Lab4_Queue/src/Lab4.java
+++ Lab4_Queue/src/Lab4.java
@@ -1,7 +1,6 @@
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-
 public class Lab4 {
     public static void main(String[] args) {
         Queue<Plane> takeoff = new LinkedBlockingQueue<Plane>();
@@ -27,31 +26,109 @@ public class Lab4 {
         /**
          * Temporary simulation vairables
          */
-        double takeoffProbability = 1d / takeoffTime;
-        double landingProbability = 1d / landingTime;
-        
+        double takeoffProbability = 1d / averageTakeoffArrival;
+        double landingProbability = 1d / averageLandingArrival;
+
         for(int currentTime = 0; currentTime < simulationTime; currentTime++) {
+            System.out.println("\nTime: " + currentTime);
+            /**
+             * This block calculates if a plane is taking off or not. If a plane
+             * is taking off, it is added to the takeoff queue.
+             */
             if(Math.random() < takeoffProbability) {
-                Plane takeoffPlane = new Plane();
+                Plane takeoffPlane = new Plane(Plane.Operation.TAKEING_OFF, currentTime);
+                takeoff.add(takeoffPlane);
                 System.out.println("Arrived for take off: " + takeoffPlane);
             } else {
                 System.out.println("Arrived for take off: ");
             }
+            /**
+             * This block calculates if a plane is landing or not. If a plane is
+             * landing, it is added to the landing queue.
+             */
             if(Math.random() < landingProbability) {
-                Plane landingPlane = new Plane();
+                Plane landingPlane = new Plane(Plane.Operation.LANDING, currentTime);
+                landing.add(landingPlane);
                 System.out.println("Arrived for landing : " + landingPlane);
             } else {
                 System.out.println("Arrived for landing : ");
             }
+            /**
+             * Calculate the runway.
+             */
+            if(runway == null) {
+                /**
+                 * Try to add a plane from the landing queue first. If the plane
+                 * has been in the air too long then mark it as crashed and find
+                 * the next plane in the landing queue. This continues until
+                 * there are no more planes in the landing queue or if an
+                 * acceptable plane was chosen to land.
+                 */
+                while(landing.peek() != null && runway == null) {
+                    runway = landing.poll();
+                    if(currentTime - runway.getArrivalTime() > maximumLandingTime) {
+                        totalCrashed++;
+                        System.out.println(
+                                runway + " has [CRASHED]."+ 
+                                " ArrivalTime: " + runway.getArrivalTime());
+                        runway = null;
+                    }
+                }
+                /**
+                 * If there wasn't a plane that needed to land, check the takeoff queue for planes needing to take off.
+                 */
+                if(runway == null) {
+                    runway = takeoff.poll();
+                }
+                /**
+                 * Finally, display the status of the runway.
+                 */
+                if(runway == null) {
+                    System.out.println("Runway: Empty");
+                } else {
+                    System.out.println(
+                            "Runway: " + runway + 
+                            " " + runway.getOperation() + 
+                            " Duration: " + runway.getActiveTime());
+                }
+            } else {
+                runway.incrementActivetime();
+                System.out.print("Runway: " + runway + " " + runway.getOperation() + " Duration: " + runway.getActiveTime());
+                switch(runway.getOperation()) {
+                case LANDING:
+                    if(runway.getActiveTime() >= landingTime) {
+                        System.out.println(" (FINISHED)");
+                        totalLanded++;
+                        runway = null;
+                    } else {
+                        System.out.println();
+                    }
+                    break;
+                case TAKEING_OFF:
+                    if(runway.getActiveTime() >= takeoffTime) {
+                        System.out.println(" (FINISHED)");
+                        totalTakeoff++;
+                        runway = null;
+                    } else {
+                        System.out.println();
+                    }
+                    break;
+                default:
+                    System.out.println("Error with " + runway + ". Removing plane from runway.");
+                    runway = null;
+                    break;
+                }
+            }
         }
-        
+
         /**
          * Simulation statistics output
          */
+        System.out.println();
         System.out.println("# of planes that crashed : " + totalCrashed);
         System.out.println("# of planes that came to the runway for take off : " + totalTakeoff);
         System.out.println("# of planes that came to the runway for landing  : " + totalLanded);
         System.out.println("The average time a plane spent on the take off queue is : " + averageTakeoff);
-        System.out.println("The average time a plane spent on the landing queue is  :  " + averageLanding);
+        System.out.println("The average time a plane spent on the landing queue is  : " + averageLanding);
     }
 }
--- Lab4_Queue/src/Plane.java
+++ Lab4_Queue/src/Plane.java
@@ -1,17 +1,80 @@
 public class Plane {
-    private static int nextPlaneId = 0;
+    private static int nextPlaneIndex = 0;
+    private final Operation operation;
+    private final int arrivalTime;
     private final int id;
-    
-    public Plane() {
-        this.id = nextPlaneId;
-        nextPlaneId++;
+    private int activeTime = 1;
+
+    public Plane(Operation operation, int arrivalTime) {
+        this.operation = operation;
+        this.arrivalTime = arrivalTime;
+        this.id = nextPlaneIndex;
+        Plane.nextPlaneIndex++;
     }
-    
+
+    /**
+     * This gets the plane's current operation.
+     * 
+     * @return The operation.
+     */
+    public Operation getOperation() {
+        return operation;
+    }
+
+    /**
+     * This gets when the plane arrived at the airport.
+     * 
+     * @return The arrival time.
+     */
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    /**
+     * This will get the unique id of the plane.
+     * 
+     * @return The id;
+     */
     public int getId() {
         return id;
     }
-    
+
+    /**
+     * This will get how long the plane has been active on the runway.
+     * 
+     * @return The active time.
+     */
+    public int getActiveTime() {
+        return activeTime;
+    }
+
+    /**
+     * This will increment the planes active time by one.
+     * 
+     * @postcondition The planes active time has been incremented by one.
+     */
+    public void incrementActivetime() {
+        activeTime++;
+    }
+
+    /**
+     * Standard toString.
+     */
     public String toString() {
         return "Plane #" + id;
+    }
+
+    /**
+     * This gets the total planes used during the simulation.
+     * 
+     * @return The total planes.
+     */
+    public static int getTotalPlanes() {
+        return nextPlaneIndex;
+    }
+
+    public static enum Operation {
+        LANDING,
+        TAKEING_OFF;
     }
 }
